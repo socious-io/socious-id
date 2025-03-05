1688c128511bd5adb289a222615fcd6d4081a56a
jeyem <me@e-mahmoudi.me>
2025-03-05 20:36:41 +0330

    role back fetch by email

--- src/sql/users/fetch_by_email.sql
+++ src/sql/users/fetch_by_email.sql
@@ -1,5 +1,3 @@
-SELECT u.*, row_to_json(m1.*) AS avatar, row_to_json(m2.*) AS cover
-FROM users u
-LEFT JOIN media m1 WHERE m1.id=u.avatar_id
-LEFT JOIN media m2 WHERE m2.id=u.cover_id
+SELECT *
+FROM users
 WHERE email=$1
