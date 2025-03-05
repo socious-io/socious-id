ecccef86e6cbb72ccc3a0e9756b0f4d042fb3a46
Mohammad Hosein Balkhani <mohammadhb25@gmail.com>
2025-03-05 22:26:43 +0330

    fix: fixing media feat: adding identity and media

--- src/sql/users/fetch.sql
+++ src/sql/users/fetch.sql
@@ -1,5 +1,5 @@
-SELECT u.*, row_to_json(m1.*) AS avatar, row_to_json(m2.*)
+SELECT u.*, row_to_json(m1.*) AS avatar, row_to_json(m2.*) AS cover
 FROM users u
-LEFT JOIN media m1 WHERE m1.id=u.avatar_id
-LEFT JOIN media m2 WHERE m2.id=u.cover_id
-WHERE id IN(?)
+LEFT JOIN media m1 ON m1.id=u.avatar_id
+LEFT JOIN media m2 ON m2.id=u.cover_id
+WHERE u.id IN(?)
