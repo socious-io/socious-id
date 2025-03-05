64b48be1820b7bf9467a8bfa7aa1b2768f035637
jeyem <me@e-mahmoudi.me>
2025-03-05 20:15:47 +0330

    fix fech query
    Signed-off-by: jeyem <me@e-mahmoudi.me>

--- src/sql/users/fetch.sql
+++ src/sql/users/fetch.sql
@@ -1,5 +1,5 @@
 SELECT u.*, row_to_json(m1.*) AS avatar, row_to_json(m2.*)
 FROM users u
-LEFT JOIN media m1 WHERE m1.id=u.avatar
-LEFT JOIN media m1 WHERE m1.id=u.avatar
+LEFT JOIN media m1 WHERE m1.id=u.avatar_id
+LEFT JOIN media m2 WHERE m2.id=u.cover_id
 WHERE id IN(?)
