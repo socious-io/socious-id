c0cb44a99b4777726de7b1e4070103ac3205439a
Ehsan Mahmoudi <me@e-mahmoudi.me>
2025-03-11 22:00:56 +0330

    fix(org): get queries need to load media

--- src/sql/organizations/fetch.sql
+++ src/sql/organizations/fetch.sql
@@ -1,6 +1,6 @@
 SELECT org.*,
-row_to_json(m1.*) AS logo,
-row_to_json(m2.*) AS cover
+    row_to_json(m1.*) AS logo,
+    row_to_json(m2.*) AS cover
 FROM organizations org
 LEFT JOIN media m1 ON m1.id=org.logo_id
 LEFT JOIN media m2 ON m2.id=org.cover_id
--- src/sql/organizations/get_all_by_member.sql
+++ src/sql/organizations/get_all_by_member.sql
@@ -1,4 +1,8 @@
-SELECT org.*
+SELECT org.*,
+    row_to_json(m1.*) AS logo,
+    row_to_json(m2.*) AS cover
 FROM organizations org
 LEFT JOIN org_members om ON om.org_id=org.id
+LEFT JOIN media m1 ON m1.id=org.logo_id
+LEFT JOIN media m2 ON m2.id=org.cover_id
 WHERE om.user_id=$1
