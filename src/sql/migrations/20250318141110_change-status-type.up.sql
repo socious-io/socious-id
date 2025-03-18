5003bf08cc8eacacda19a67e159973d49dfb891a
Mohammad Hosein Balkhani <mohammadhb25@gmail.com>
2025-03-18 15:47:09 +0330

    feat: adding verification sync on org

--- src/sql/migrations/20250318141110_change-status-type.up.sql
+++ src/sql/migrations/20250318141110_change-status-type.up.sql
@@ -13,3 +13,18 @@ ALTER TABLE organizations
     ALTER COLUMN status DROP DEFAULT,
     ALTER COLUMN status TYPE status_type USING status::text::status_type,
     ALTER COLUMN status SET DEFAULT 'NOT_ACTIVE';
+
+-- Synchronize Verification With Status
+CREATE OR REPLACE FUNCTION sync_organization_verification()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF NEW.verified IS TRUE OR NEW.verified_impact IS TRUE THEN
+        NEW.status = 'ACTIVE';
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE TRIGGER trigger_sync_organization_verification
+BEFORE INSERT OR UPDATE ON organizations
+FOR EACH ROW EXECUTE FUNCTION sync_organization_verification();
