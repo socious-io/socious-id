10fb8370fc773b5d7020f60a6ca912b9f0aa74eb
Mohammad Hosein Balkhani <mohammadhb25@gmail.com>
2025-05-04 18:37:25 +0330

    feat: integrated impact_points to user entity

--- src/sql/migrations/20250502193903_update-body-type-credentials.up.sql
+++ src/sql/migrations/20250502193903_update-body-type-credentials.up.sql
@@ -1,2 +1,25 @@
 ALTER TABLE verification_credentials
 ALTER COLUMN body TYPE jsonb USING body::jsonb;
+
+-- Impact Point user integration
+ALTER TABLE users
+ADD COLUMN impact_points int DEFAULT 0 NOT NULL;
+
+CREATE OR REPLACE FUNCTION update_user_impact_points() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$
+BEGIN
+  UPDATE users SET impact_points = impact_points + NEW.total_points WHERE id=NEW.user_id;
+  RETURN NEW;
+END;
+$$;
+CREATE OR REPLACE TRIGGER update_user_impact_points AFTER INSERT ON impact_points FOR EACH ROW EXECUTE FUNCTION update_user_impact_points();
+
+UPDATE users u
+SET impact_points = sub.total
+FROM (
+  SELECT user_id, SUM(total_points) AS total
+  FROM impact_points
+  GROUP BY user_id
+) AS sub
+WHERE sub.user_id = u.id;
