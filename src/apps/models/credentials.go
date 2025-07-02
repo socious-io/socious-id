0eeb3cb719faa8b273df663b2f3ff29d64f278a8
Mohammad Hosein Balkhani <mohammadhb25@gmail.com>
2025-07-02 13:30:22 +0330

    fix: add credential type

--- src/apps/models/credentials.go
+++ src/apps/models/credentials.go
@@ -177,6 +177,7 @@ func (v *Credential) SendBadges(ctx context.Context) error {
 	}
 
 	vc, err := wallet.SendCredentials(*v.ConnectionID, config.Config.Wallet.AgentTrustDID, wallet.H{
+		"type":   "impact_point_badges",
 		"badges": badges,
 	})
 	if err != nil {
