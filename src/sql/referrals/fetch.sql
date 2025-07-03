ec59e17470386a8364b6fa10299d16ed35177717
Mohammad Hosein Balkhani <mohammadhb25@gmail.com>
2025-07-03 19:25:50 +0330

    fix: fixing referral query

--- src/sql/referrals/fetch.sql
+++ src/sql/referrals/fetch.sql
@@ -1,16 +1,12 @@
-WITH ra AS (
-  SELECT *
-  FROM referral_achievements
-  WHERE referee_id IN (?)
-)
 SELECT
 	COALESCE(jsonb_agg(
 		json_build_object(
-		  'type', ra.achievement_type, 
-		  'reward_claimed_at', ra.reward_claimed_at
+			'type', ra.achievement_type, 
+			'reward_claimed_at', ra.reward_claimed_at
 		)
-	), '[]') AS achievements,
+  	) FILTER (WHERE ra IS NOT NULL), '[]') AS achievements,
 	row_to_json(i.*) as referee
-FROM ra
-JOIN identities i ON i.id=ra.referee_id
+FROM identities i
+LEFT JOIN referral_achievements ra ON ra.referee_id=i.id
+WHERE i.id IN (?)
 GROUP BY i.id;
