5290d80ae8b0e323db2fa6f9919f0679a02d3542
Mohammad Hosein Balkhani <mohammadhb25@gmail.com>
2025-07-09 13:44:47 +0330

    fix: fixed fetching referral achievement query

--- src/sql/referrals/fetch.sql
+++ src/sql/referrals/fetch.sql
@@ -4,9 +4,10 @@ SELECT
 			'type', ra.achievement_type, 
 			'reward_claimed_at', ra.reward_claimed_at
 		)
-  	) FILTER (WHERE ra IS NOT NULL), '[]') AS achievements,
+  	) FILTER (WHERE ra.id IS NOT NULL), '[]') AS achievements,
 	row_to_json(i.*) as referee
 FROM identities i
-LEFT JOIN referral_achievements ra ON ra.referee_id=i.id
+LEFT JOIN users u ON u.id=i.id
+LEFT JOIN referral_achievements ra ON (ra.referrer_id = u.referred_by AND ra.referee_id=i.id)
 WHERE i.id IN (?)
 GROUP BY i.id;
