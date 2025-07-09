9d420f36806b703778abc1ed6e589f3550af381a
Mohammad Hosein Balkhani <mohammadhb25@gmail.com>
2025-07-09 14:02:19 +0330

    Merge main to prod (#53)
    * fix: fixing KYB review message on discord
    
    * fix: fixed a error was not being shown whenever user tried to create an organization with same username
    
    * kyc pass for dev env on any wallet
    
    * fix: fix KYC not getting present proof
    
    * fix: fixed KYC credential shouldn't get present proof before the callback
    
    * fix: fixed fetching referral achievement query
    
    ---------
    
    Co-authored-by: Shant Isaghoolian <shant@Shants-MacBook-Air.local>

--- src/sql/referrals/get_all_achievements.sql
+++ src/sql/referrals/get_all_achievements.sql
@@ -1,4 +1,4 @@
 SELECT id, COUNT(*) OVER () as total_count 
 FROM referral_achievements
-WHERE referrer_id=$1
+WHERE (referrer_id = $1 OR (referee_id = $1 AND referrer_id IS NULL))
 LIMIT $2 OFFSET $3
