86f65b66f17b7bf8c90932bc7bab43a1470ccc95
Mohammad Hosein Balkhani <mohammadhb25@gmail.com>
2025-07-11 02:15:34 +0330

    feat: add ability for admin to claim all of the user's achievements

--- src/apps/views/referrals.go
+++ src/apps/views/referrals.go
@@ -8,6 +8,7 @@ import (
 	"socious-id/src/apps/utils"
 
 	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
 	database "github.com/socious-io/pkg_database"
 )
 
@@ -82,9 +83,24 @@ func referralsGroup(router *gin.Engine) {
 		})
 	})
 
-	g.POST("achievements/claim", auth.LoginRequired(), paginate(), func(c *gin.Context) {
-		//Pay the rewards
-		//Set all the rewards as claimed
-		panic("not implemented")
+	g.POST("achievements/claim", adminAccessRequired(), func(c *gin.Context) {
+		ctx := c.MustGet("ctx").(context.Context)
+		identityId := uuid.MustParse(c.Query("identity_id"))
+
+		_, err := models.GetIdentity(identityId)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		err = models.ClaimAllReferralAchievements(ctx, identityId)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, gin.H{
+			"success": true,
+		})
 	})
 }
