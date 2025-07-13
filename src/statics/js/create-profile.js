ce6bcfeeffac9194d921e44187d408b3b5e1ddd7
Mohammad Hosein Balkhani <mohammadhb25@gmail.com>
2025-07-13 17:45:34 +0330

    fix: added username validation and shortname validation and optimized it

--- src/statics/js/create-profile.js
+++ src/statics/js/create-profile.js
@@ -46,25 +46,35 @@ const onUploadProfile = async () => {
 };
 
 const validateForm = () => {
-	const first = document.getElementById("first").value.trim();
-	const last = document.getElementById("last").value.trim();
-	const username = document.getElementById("username").value.trim();
-	const password = document.getElementById("pass").value.trim();
-	const lengthHint = document.getElementById("length-hint");
-	const charHint = document.getElementById("character-hint");
-	const isPasswordValid = password.length >= 8 && /[\W_]/g.test(password);
-	const submitBtn = document.querySelector("form button[type='submit']");
+    const first = document.getElementById("first").value.trim();
+    const last = document.getElementById("last").value.trim();
+    const usernameInput = document.getElementById("username");
+    const username = usernameInput.value.trim();
+    const errorSpan = document.getElementById("username-error");
+    const password = document.getElementById("pass").value.trim();
+    const lengthHint = document.getElementById("length-hint");
+    const charHint = document.getElementById("character-hint");
+    const submitBtn = document.querySelector("form button[type='submit']");
+    
+    //Password Validation
+    const isPasswordValid = password.length >= 8 && /[\W_]/g.test(password);
+    lengthHint.src = `/statics/icons/check-${password.length >= 8 ? 'green': 'grey'}.svg`;
+    charHint.src = `/statics/icons/check-${/[\W_]/.test(password) ? 'green': 'grey'}.svg`;
 
-	lengthHint.src = `/statics/icons/check-${password.length >= 8 ? "green" : "grey"}.svg`;
+    //Username Validation
+    const isUsernameValid = username && usernameInput.checkValidity();
+    if (!usernameInput.checkValidity()) {
+        errorSpan.style.display = "block";
+    } else {
+        errorSpan.style.display = "none";
+    }
 
-	charHint.src = `/statics/icons/check-${/[\W_]/.test(password) ? "green" : "grey"}.svg`;
-
-	if (first && last && username && isPasswordValid) {
-		submitBtn.removeAttribute("disabled");
-	} else {
-		submitBtn.setAttribute("disabled", "true");
-	}
-};
+    if(first && last && isUsernameValid && isPasswordValid) {
+        submitBtn.removeAttribute("disabled");
+    } else {
+        submitBtn.setAttribute("disabled", "true");
+    }
+}
 
 const createProfile = () => {
 	const first_name = document.getElementById("first").value.trim();
@@ -154,17 +164,29 @@ const onUploadLogo = async () => {
 
 const validateOrgForm = () => {
 	const name = document.getElementById("name").value.trim();
-	const shortname = document.getElementById("shortname").value.trim();
-	const email = document.getElementById("email").value.trim();
+	const shortnameInput = document.getElementById("shortname");
+    const shortname = shortnameInput.value.trim();
+	const errorSpan = document.getElementById("shortname-error");
+	const emailInput = document.getElementById("email");
+	const email = emailInput.value.trim();
 	const submitBtn = document.querySelector("#submit");
 
-	if (name && email && shortname) {
+    //Shortname Validation
+    const isShortnameValid = shortname && shortnameInput.checkValidity();
+    if (!shortnameInput.checkValidity()) {
+        errorSpan.style.display = "block";
+    } else {
+        errorSpan.style.display = "none";
+    }
+
+	//Email Validation
+	const isEmailValid = email && emailInput.checkValidity();
+
+	if (name && isShortnameValid && isEmailValid) {
 		submitBtn.removeAttribute("disabled");
 	} else {
 		submitBtn.setAttribute("disabled", "true");
 	}
-
-	
 };
 
 const createOrganization = (e) => {
